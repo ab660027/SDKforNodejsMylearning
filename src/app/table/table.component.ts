e16ea046b424f1ad03700a173b9eeec6e8409fa8
AB660027 <AB660027@jp.ibm.com>
2017-03-10 18:52:23 +0900

    test table component #001

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -11,9 +11,16 @@ import { Dto } from '../dto';
   providers: [SearchService]
 })
 export class TableComponent implements OnInit {
+  public per: number;
+  public page: number;
+  public pager: (string | number)[];
   public datas: Dto[];
 
-  constructor(private searchService: SearchService) { }
+  constructor(private searchService: SearchService) {
+    this.per = 5;
+    this.page = 1;
+    this.pager = [1, 2, 3, 4, 5, 6, '>'];
+  }
 
   ngOnInit() { this.getData(); }
 
@@ -21,4 +28,30 @@ export class TableComponent implements OnInit {
     this.searchService.getData().subscribe((val: Dto[]) => this.datas = val, (err: any) => console.log(err), () => console.log(this.datas));
   }
 
+  onClick(page: string | number) {
+    if (page === '<' && this.page > 1) {
+      this.page--;
+      if (this.page >= 3) {
+        for (let i = 0; i < 7; i++) {
+          //          typeof this.pager[i] === 'number' ? 
+        }
+      }
+    }
+    if (page === '>' && this.page * this.per < this.datas.length) {
+      this.page++;
+    }
+    if (typeof page === 'number') {
+      this.page = page;
+    }
+    this.pager = [];
+    if (page > 1) {
+      this.pager.push('<');
+    }
+    for (var i = this.page - 7; this.pager.length < 7; i++) {
+      if (i < 1) {
+        continue;
+      }
+      this.pager.push(i);
+    }
+  }
 }
